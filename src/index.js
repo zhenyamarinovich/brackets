bc8446dd1bd6c8aa56f739370884a9f69754731a
Evgeniy Marinovich <zmarinovich@mail.ru>
2020-10-12 07:58:50 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let stack = [];
+  
+  let openBrackets = bracketsConfig.map(item => item[0]);
+  let closeBrackets = bracketsConfig.map(item => item[1]);
+
+  str.split('').forEach(item => {
+    if (openBrackets.includes(item) && closeBrackets.includes(item)) {
+      if (stack.length && stack[stack.length - 1] === item) {
+        stack.pop();
+      } else {
+        stack.push(item);
+      }      
+    } else if (openBrackets.includes(item)) {
+      stack.push(item);
+    } else if (stack.length && openBrackets.indexOf(stack[stack.length - 1]) === closeBrackets.indexOf(item)) {
+      stack.pop();
+    } else {
+      stack.push(item);
+    }    
+  }); 
+
+  return stack.length === 0;
 }
